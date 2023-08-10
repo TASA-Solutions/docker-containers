8f6ed3eb905ece62a0a434a40664a693e89ac0f7
arshadazaad3 <arshad.azaad32@gmail.com>
2023-08-10 14:16:22 +0530

    initial commit - mongodb replica local

---
+++ db/mongodb_replica_local/dir-init.sh
@@ -0,0 +1,5 @@
+mkdir dockerdata
+cd dockerdata
+mkdir mongo.one.vol
+mkdir mongo.two.vol
+mkdir mongo.three.vol
---
+++ db/mongodb_replica_local/replica-init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Running initialization script..."
+
+# Execute your MongoDB setup command
+docker exec -it mongo.one.db mongosh --eval "rs.initiate({_id:'dbrs', members: [{_id:0, host: 'mongo.one.db'},{_id:1, host: 'mongo.two.db'},{_id:2, host: 'mongo.three.db'}]})"
+
+echo "Initialization script completed."
