b9e3858cf4718823fb65f454d7176393848bfdcb
arshadazaad3 <arshad.azaad32@gmail.com>
2023-08-20 12:41:39 +0530

    updated URL

--- db/mongodb_replica_local/2.replica-init.sh
+++ db/mongodb_replica_local/2.replica-init.sh
@@ -4,7 +4,12 @@
 
 echo "Running initialization script..."
 
+sleep 10
+
 # Execute your MongoDB setup command
 docker exec -it mongo.one.db mongosh --eval "rs.initiate({_id:'dbrs', members: [{_id:0, host: 'mongo.one.db'},{_id:1, host: 'mongo.two.db'},{_id:2, host: 'mongo.three.db'}]})"
 
 echo "Initialization script completed."
+
+## connection URL
+# mongodb://localhost:2717/?directConnection=true
